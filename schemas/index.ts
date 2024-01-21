a16a1877de04e45f5e724116e6b6ed688d4efd99
LarinNikita <larin_nikita_m@mail.ru>
2024-01-21 16:55:06 +0700

    settings form

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
@@ -36,4 +37,29 @@ export const NewPasswordSchema = z.object({
 
 export const SettingsSchema = z.object({
     name: z.optional(z.string()),
-});
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.string().email(),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+})
+    .refine((data) => {
+        if (data.password && !data.newPassword) {
+            return false;
+        }
+
+        return true;
+    }, {
+        message: "New password is required!",
+        path: ["newPassword"]
+    })
+    .refine((data) => {
+        if (data.newPassword && !data.password) {
+            return false;
+        }
+
+        return true;
+    }, {
+        message: "Password is required!",
+        path: ["password"]
+    });
